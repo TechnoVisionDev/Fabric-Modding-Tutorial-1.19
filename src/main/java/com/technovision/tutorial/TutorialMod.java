4a2e25502254420d1353599e062dda5213cef6cb
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-07-19 11:42:38 -0700

    Add javadoc

--- src/main/java/com/technovision/tutorial/TutorialMod.java
+++ src/main/java/com/technovision/tutorial/TutorialMod.java
@@ -3,6 +3,11 @@ package com.technovision.tutorial;
 import com.technovision.tutorial.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+/**
+ * Main class where our mod is initialized.
+ *
+ * @author TechnoVision
+ */
 public class TutorialMod implements ModInitializer {
 
     public static final String MOD_ID = "tutorial";
